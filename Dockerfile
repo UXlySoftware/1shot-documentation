33e9596f6fad0385c63f187b8508ee039b1889de
TtheBC01 <tac688@alumni.stanford.edu>
2025-02-12 22:26:50 -0800

    trying pydata theme

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM sphinxdoc/sphinx
+
+RUN python -m pip install pydata-sphinx-theme
--- source/conf.py
+++ source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = '1Shot API Docs'
+project = '1Shot API'
 copyright = '2025, UXly Software'
 author = 'UXly Software'
 release = '0.1.0'
@@ -26,6 +26,11 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
+html_title = '1Shot Docs'
+#html_logo = '' # https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-html_logo
+#html_favicon = '' # https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-html_favicon
+html_show_sphinx = False
+html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
 html_baseurl = 'https://docs.1shotapi.com'
