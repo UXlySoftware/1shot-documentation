df154ec2794aee27b2703406d9dc7b6c76a159bb
TtheBC01 <tac688@alumni.stanford.edu>
2025-02-27 10:33:26 -0800

    github and X links

--- source/conf.py
+++ source/conf.py
@@ -34,3 +34,27 @@ html_show_sphinx = False
 html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
 html_baseurl = 'https://docs.1shotapi.com'
+html_theme_options = {
+    "icon_links": [
+        {
+            # Label for this link
+            "name": "GitHub",
+            # URL where the link will redirect
+            "url": "https://github.com/uxlySoftware/1shot-documentation",  # required
+            # Icon class (if "type": "fontawesome"), or path to local image (if "type": "local")
+            "icon": "fa-brands fa-github",
+            # The type of image to be used (see below for details)
+            "type": "fontawesome",
+        },
+        {
+            # Label for this link
+            "name": "X",
+            # URL where the link will redirect
+            "url": "https://x.com/uxlysoftware",  # required
+            # Icon class (if "type": "fontawesome"), or path to local image (if "type": "local")
+            "icon": "fa-brands fa-x-twitter",
+            # The type of image to be used (see below for details)
+            "type": "fontawesome",
+        }
+   ]
+}
